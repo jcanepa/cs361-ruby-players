f8390a8724b7197593a67a9fe30ab242922a6a5a
Julian Canepa <julian.l.canepa@gmail.com>
2022-09-22 21:47:24 -0700

    Remove spaces from main.rb main function.

--- main.rb
+++ main.rb
@@ -4,10 +4,8 @@ def run()
     players = [];
     50.times {
         |i|
-
         index_plus_one = i + 1
         random_score = rand(10...300)
-
         players << Player.new(
                             "Player #{index_plus_one}",
                             random_score)
