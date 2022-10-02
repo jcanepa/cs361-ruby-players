5699c26a7047063f64cdaa515574d80b878b13af
Julian Canepa <julian.l.canepa@gmail.com>
2022-10-02 11:57:12 -0700

    Update main function to use do/end in place of brackets. Add spacing for readability.

--- main.rb
+++ main.rb
@@ -1,19 +1,21 @@
 require_relative "player"
 
 def run()
+
     players = [];
+
     50.times do |i|
         index_plus_one = i + 1
         random_score = rand(10...300)
+
         players << Player.new(
                             "Player #{index_plus_one}",
                             random_score)
     end
 
-    players.each {
-        |player|
+    players.each do |player|
         puts  "Ready Player #{player.name.to_s}! Score: #{player.score.to_s}"
-    }
+    end
 end
 
 run()
